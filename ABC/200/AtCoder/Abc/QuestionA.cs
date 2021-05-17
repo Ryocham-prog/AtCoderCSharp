0c2135f3278ee4586df6d887359acf473cd02e9b
Ryocham <ryocham.prog@gmail.com>
2021-05-17 23:32:04 +0900

    Add 200 project and Add questionA,B solution

---
+++ ABC/200/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc200/tasks/abc200_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+                Console.SetOut(sw);
+
+                // 整数の入力
+                long n = long.Parse(Console.ReadLine());
+                var century = ((n - 1) / 100) + 1;
+
+                Console.WriteLine(century.ToString());
+            }
+            finally
+            {
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/200/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc200/tasks/abc200_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+                Console.SetOut(sw);
+
+                // 整数配列の入力
+                var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+                var n = inputLongArray[0];
+                var k = inputLongArray[1];
+
+                for (int i = 0; i < k; i++)
+                {
+                    if (n % 200 == 0)
+                    {
+                        n /= 200;
+                    }
+                    else
+                    {
+                        var tmp = n.ToString() + "200";
+                        n = long.Parse(tmp);
+                    }
+                }
+
+                Console.WriteLine(n.ToString());
+            }
+            finally
+            {
+                Console.Out.Flush();
+            }
+        }
+    }
+}
