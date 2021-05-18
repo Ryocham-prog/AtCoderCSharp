01100e1d61bc97c5b80e4fcefde19705a4c08259
Ryocham <ryocham.prog@gmail.com>
2021-05-18 23:34:09 +0900

    Add 199 project and Add questionA,B solution

---
+++ ABC/199/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc199/tasks/abc199_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+                Console.SetOut(sw);
+
+                // 整数配列の入力
+                var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+                var a = inputLongArray[0];
+                var b = inputLongArray[1];
+                var c = inputLongArray[2];
+
+                var isYes = (a * a + b * b) < (c * c);
+                Console.WriteLine(isYes ? "Yes" : "No");
+            }
+            finally
+            {
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/199/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc199/tasks/abc199_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+                Console.SetOut(sw);
+
+                // 数列の長さの入力
+                long n = long.Parse(Console.ReadLine());
+
+                // 数列Aの入力
+                var arrayA = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+
+                // 数列Bの入力
+                var arrayB = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+
+                var aMax = arrayA.Max();
+                var bMin = arrayB.Min();
+
+                if (aMax > bMin)
+                {
+                    Console.WriteLine("0");
+                    return;
+                }
+
+                var xCount = Enumerable.Range(aMax, bMin - aMax +1).Count() ;
+                Console.WriteLine(xCount.ToString());
+            }
+            finally
+            {
+                Console.Out.Flush();
+            }
+        }
+    }
+}
