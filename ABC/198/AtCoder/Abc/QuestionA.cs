898a5ffeb08051aa12b5f1dc20d732ff0d62f5e0
Ryocham <ryocham.prog@gmail.com>
2021-05-20 02:06:12 +0900

    Add 198 project and Add questionA,B solution

---
+++ ABC/198/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc198/tasks/abc198_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+                Console.SetOut(sw);
+
+                // 整数の入力
+                long n = long.Parse(Console.ReadLine());
+
+                if (n < 2)
+                {
+                    Console.WriteLine(0);
+                    return;
+                }
+
+                Console.WriteLine(n - 1);
+            }
+            finally
+            {
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/198/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc198/tasks/abc198_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+                Console.SetOut(sw);
+
+                // 文字列の入力
+                string s = Console.ReadLine();
+
+                var len = s.Length;
+                var nStr = new StringBuilder(s);
+                for (int i = 0; i < len; i++)
+                {
+                    if (s.Substring(len - 1 - i, 1) != "0") break;
+                    nStr.Insert(0, "0");
+                }
+
+                var target = nStr.ToString();
+                var targetLen = target.Length;
+                var loopCnt = targetLen / 2;
+                for (int i = 0; i < loopCnt; i++)
+                {
+                    if (target.Substring(i, 1) != target.Substring(targetLen - 1 - i, 1))
+                    {
+                        Console.WriteLine("No");
+                        return;
+                    }
+                }
+
+                Console.WriteLine("Yes");
+            }
+            finally
+            {
+                Console.Out.Flush();
+            }
+
+        }
+    }
+}
