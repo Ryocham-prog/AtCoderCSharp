bdc8054f58894c59869f9f1069f3bfd46ee6d961
Ryocham <ryocham.prog@gmail.com>
2021-05-24 01:07:45 +0900

    Add 196 project and Add questionA,B solution

---
+++ ABC/196/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc196/tasks/abc196_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false })
+            {
+
+                Console.SetOut(sw);
+
+                // xの範囲の入力
+                var xRange = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+                // yの範囲の入力
+                var yRange = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+                // xの最大値からyの最小値を引いて出力
+                Console.WriteLine(xRange[1]-yRange[0]);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/196/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false })
+            {
+                Console.SetOut(sw);
+
+                // Xの入力
+                var x = Console.ReadLine().ToString();
+                
+                if(x.IndexOf(".") == -1)
+                {
+                    Console.WriteLine(x);
+                    Console.Out.Flush();
+                    return;
+                }
+
+                var splitX = x.Split('.');
+                Console.WriteLine(splitX[0]);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
