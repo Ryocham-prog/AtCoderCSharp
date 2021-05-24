a34363be7a4dc0473997fdbf82ccf9d08758c501
Ryocham <ryocham.prog@gmail.com>
2021-05-24 23:58:49 +0900

    Add 195 project and Add questionA,B solution

---
+++ ABC/195/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc195/tasks/abc195_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false })
+            {
+                Console.SetOut(sw);
+
+                // MとHの入力
+                var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+                var result = inputLongArray[1] % inputLongArray[0] == 0 ? "Yes" : "No";
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/195/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc195/tasks/abc195_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false })
+            {
+                Console.SetOut(sw);
+
+                // A,B,Wの入力
+                var inputDecimalArray = Console.ReadLine().Split(' ').Select(i => decimal.Parse(i)).ToArray();
+
+                var a = inputDecimalArray[0];
+                var b = inputDecimalArray[1];
+                var w = inputDecimalArray[2] * 1000;
+
+                var min = Math.Ceiling(w / b) ;
+                var max = Math.Floor(w / a);
+
+                var result = min > max ? "UNSATISFIABLE" : $"{min} {max}";
+
+                Console.WriteLine(result);
+                Console.Out.Flush();
+            }
+        }
+    }
+}
