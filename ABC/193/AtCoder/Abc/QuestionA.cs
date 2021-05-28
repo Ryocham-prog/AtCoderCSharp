9c7767f45fc02d17bf1e999c727d89b47095aae6
Ryocham <ryocham.prog@gmail.com>
2021-05-29 01:03:35 +0900

    Add 193 project and Add questionA solution

---
+++ ABC/193/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc193/tasks/abc193_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // A,Bの入力
+                var inputDoubleArray = Console.ReadLine().Split(' ').Select(i => double.Parse(i)).ToArray();
+                var discount = (1 - (inputDoubleArray[1] / inputDoubleArray[0])) * 100;
+                Console.WriteLine(discount);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
