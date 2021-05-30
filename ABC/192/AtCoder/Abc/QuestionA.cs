be20be03042e2e34dbd5d73d64644d7ec58f32a5
Ryocham <ryocham.prog@gmail.com>
2021-05-30 14:24:05 +0900

    Add 195 project and Add questionA,B solution

---
+++ ABC/192/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc192/tasks/abc192_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // コインの枚数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                Console.WriteLine((100 - n % 100).ToString());
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/192/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+
+                Console.SetOut(sw);
+
+                // 文字列の入力
+                var inputArray = Console.ReadLine().ToArray()
+                    .Select((input, idx) => new {input , Index = idx});
+
+                // 奇数番目の文字列の判定
+                var oddArray = inputArray.Where(x => (x.Index + 1) % 2 == 1).Where(x => Char.IsLower(x.input) == false);
+                if(oddArray.Count() > 0)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+                // 偶数番目の文字列の判定
+                var evenArray = inputArray.Where(x => (x.Index + 1) % 2 == 0).Where(x => Char.IsUpper(x.input) == false);
+                if (evenArray.Count() > 0)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+                Console.WriteLine("Yes");
+                Console.Out.Flush();
+            }
+        }
+    }
+}
