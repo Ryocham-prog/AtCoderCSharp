3c5be634a89c33af839ecb95efa6ba12a521801b
Ryocham <ryocham.prog@gmail.com>
2021-05-31 22:43:00 +0900

    Add 191 project and Add questionA,B solution

---
+++ ABC/191/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc191/tasks/abc191_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 情報(速さV, 消え始めT, 消え終わりS, 判定位置D)の入力
+                var inputintArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                var v = inputintArray[0];
+                var t = inputintArray[1];
+                var s = inputintArray[2];
+                var d = inputintArray[3];
+
+                var isYes = (d < v * t || v * s < d) ? "Yes" : "No";
+                Console.WriteLine(isYes);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/191/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N,Xの入力
+                var inputintArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+
+                var inputN = inputintArray[0];
+                var inputX = inputintArray[1].ToString();
+
+                // 整数列Aの入力
+                var targetArray = Console.ReadLine().Split(' ')
+                    .Where(a => a != inputX).ToArray();
+
+                if (targetArray.Length == 0)
+                {
+                    Console.WriteLine();
+                    return;
+                }
+
+                Console.WriteLine(String.Join(" ", targetArray));
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
