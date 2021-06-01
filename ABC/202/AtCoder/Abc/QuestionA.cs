fc76e8662d0c71a88843a236cc69f3e20882efb2
Ryocham <ryocham.prog@gmail.com>
2021-06-02 01:26:58 +0900

    Add 202 project and Add questionA,B solution

---
+++ ABC/202/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    //https://atcoder.jp/contests/abc141/tasks/abc141_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数配列の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+
+                if (inputArray.Length != 3) return;
+
+                var sum = inputArray.Sum(x => 7 - x);
+                Console.WriteLine(sum);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/202/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 文字列Sの入力
+                var inputArray = Console.ReadLine().ToArray();
+
+                if (inputArray.Length == 0) return;
+
+                var qb = new QuestionB();
+                var result = inputArray.Select(input => input = qb.ReplaceStr(input)).Reverse().ToArray();
+                Console.WriteLine(String.Join("", result));
+
+                Console.Out.Flush();
+            }
+        }
+
+        public char ReplaceStr(char target)
+        {
+            if(target == '6')
+            {
+                return '9';
+            }
+
+            if (target == '9')
+            {
+                return '6';
+            }
+
+            return target;
+        }
+    }
+}
