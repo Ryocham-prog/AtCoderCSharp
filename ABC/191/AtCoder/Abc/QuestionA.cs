e8058a9f8d6a67640322f851c63a8a449027639b
Ryocham <ryocham.prog@gmail.com>
2021-06-03 20:23:10 +0900

    Update 191-questionA solutions

--- ABC/191/AtCoder/Abc/QuestionA.cs
+++ ABC/191/AtCoder/Abc/QuestionA.cs
@@ -17,6 +17,13 @@ namespace AtCoder.Abc
 
                 // 情報(速さV, 消え始めT, 消え終わりS, 判定位置D)の入力
                 var inputintArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+
+                if(inputintArray.Length != 4)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"V T S D\")");
+                    return;
+                }
+
                 var v = inputintArray[0];
                 var t = inputintArray[1];
                 var s = inputintArray[2];
