5308eac90dc367351ca183088e724256ec9e7cf6
Ryocham <ryocham.prog@gmail.com>
2021-06-04 00:13:10 +0900

    Add 203 project and Add questionA,B solution

---
+++ ABC/203/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc203/tasks/abc203_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数配列の入力
+                var inputArray = Console.ReadLine().Split(' ').ToArray();
+                if(inputArray.Length != 3)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"a b c\")");
+                    return;
+                }
+
+                var filterArray = inputArray.GroupBy(x => x).Where(x => x.Count() == 1).ToList();
+                // 1つも同じ目がない場合
+                if(filterArray.Count == 3)
+                {
+                    Console.WriteLine("0");
+                    return;
+                }
+
+                // 3つとも同じ目の場合
+                if (filterArray.Count == 0)
+                {
+                    Console.WriteLine(inputArray[0]);
+                    return;
+                }
+
+                // 2つ同じ目の場合
+                Console.WriteLine(filterArray[0].Key);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/203/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    class QuestionB
+    {
+        // https://atcoder.jp/contests/abc203/tasks/abc203_b
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 階層Nと部屋数Kの入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if(inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"N K\")");
+                }
+
+                var floorInput = inputArray[0];
+                var roomInput = inputArray[1];
+
+                // 1階だけの合計値を算出(a)
+                var firstFloorSum = Enumerable.Range(101, roomInput).Sum();
+                if(floorInput == 1)
+                {
+                    Console.WriteLine(firstFloorSum.ToString());
+                    return;
+                }
+
+                //  2階以上の場合、100の位の増加分を考慮して、
+                //  100 + 部屋数 * (階層 - 1)を足す
+                var highFloorHundredsSum = Enumerable.Range(2, floorInput - 1)
+                    .Select(n => 100 * roomInput * (n - 1)).Sum();
+
+                var allFloorSum = firstFloorSum * floorInput + highFloorHundredsSum;
+                Console.WriteLine(allFloorSum.ToString()) ;
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
