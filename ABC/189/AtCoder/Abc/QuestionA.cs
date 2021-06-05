b974963baab17b0730a3790c4b4021a581a4d515
Ryocham <ryocham.prog@gmail.com>
2021-06-06 03:18:53 +0900

    Add 189 project and Add questionA,B solution

---
+++ ABC/189/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    //https://atcoder.jp/contests/abc141/tasks/abc141_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 文字列配列の入力
+                var inputArray = Console.ReadLine().ToArray();
+
+                var filterArray = inputArray.GroupBy(x => x).Where(x => x.Count() == 3).ToArray();
+                var result = filterArray.Count() == 1 ? "Won" : "Lost";
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/189/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:飲んだお酒の総数, X:酔っぱらうアルコール摂取量の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if(inputArray.Length !=2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"N X\")");
+                    return;
+                }
+
+                var n = inputArray[0];
+                var x = inputArray[1] * 100;    // 整数で計算するため、100倍する
+
+                // 飲んだお酒の情報(V:お酒の量, P:アルコール度数(%))の入力
+                var alcoholInfoArray = Enumerable.Range(1, n)
+                    .Select(input => Console.ReadLine())
+                    .Select((input, index) => new { v = int.Parse(input.Split(' ')[0]), p = int.Parse(input.Split(' ')[1]), index }).ToArray();
+
+                var alcoholSum = 0;
+                foreach(var info in alcoholInfoArray)
+                {
+                    alcoholSum += info.v * info.p;
+                    if(alcoholSum > x)
+                    {
+                        Console.WriteLine((info.index + 1).ToString());
+                        return;
+                    }
+                }
+
+                Console.WriteLine("-1");
+                Console.Out.Flush();
+            }
+        }
+    }
+}
