a189922a78d55ffad5876186742104238c2e1012
Ryocham <ryocham.prog@gmail.com>
2021-06-07 01:47:42 +0900

    Add 188 project and Add questionA,B solution

---
+++ ABC/188/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc188/tasks/abc188_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数配列の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"X Y\")");
+                    return;
+                }
+
+                var result = Math.Abs(inputArray[0] - inputArray[1]) >= 3 ? "No" : "Yes";
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/188/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc188/tasks/abc188_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+
+                Console.SetOut(sw);
+
+                // N:次元の入力
+                var n = int.Parse(Console.ReadLine());
+
+                // ベクトルAの入力
+                var inputArrayA = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArrayA.Length != n)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A1 A2 A3 … An\")");
+                    return;
+                }
+
+                // ベクトルBの入力
+                var inputArrayB = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArrayB.Length != n)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"B1 B2 B3 … Bn\")");
+                    return;
+                }
+
+                var sumAB = Enumerable.Range(0, n)
+                    .Select(x => inputArrayA[x] * inputArrayB[x])
+                    .Sum();
+
+                var result = sumAB == 0 ? "Yes" : "No";
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
