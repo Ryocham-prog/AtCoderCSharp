faf148420bc39c9ab69d277b234a0fccc2960c2d
Ryocham <ryocham.prog@gmail.com>
2021-06-08 00:10:36 +0900

    Add 187 project and Add questionA,B solution

---
+++ ABC/187/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc187/tasks/abc187_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 文字列配列の入力
+                var inputArray = Console.ReadLine().Split(' ');
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A B\")");
+                    return;
+                }
+
+                var sumA = inputArray[0].ToArray()
+                    .Select(x => int.Parse(x.ToString()))
+                    .Sum();
+
+                var sumB = inputArray[1].ToArray()
+                    .Select(x => int.Parse(x.ToString()))
+                    .Sum();
+
+                var result = sumA >= sumB ? sumA : sumB;
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/187/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:点の数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                // x,y:点iの座標の入力
+                var inputArray = Enumerable.Range(1, n)
+                    .Select(x => Console.ReadLine())
+                    .Select(input => new { inputX = decimal.Parse(input.Split(' ')[0]), inputY = decimal.Parse(input.Split(' ')[1])}).ToArray();
+
+                // (x1,y1)(x2,y2)のバリエーションを配列化
+                var patternList = inputArray
+                    .SelectMany(
+                    (xy1, index) => inputArray.Skip(index+1).Select(xy2 => new { xy1, xy2 })
+                    ).ToArray();
+
+                var result = patternList
+                    .Select(data => ((data.xy2.inputY - data.xy1.inputY) / (data.xy2.inputX - data.xy1.inputX)))
+                    .Where(data => (data >= -1 && data <= 1))
+                    .Count();
+
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
