61439ffd9c04ddbdb4f360df3bdf57655bb4ebaa
Ryocham <ryocham.prog@gmail.com>
2021-06-08 22:48:21 +0900

    Add 186 project and Add questionA,B solution

---
+++ ABC/186/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc186/tasks/abc186_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:トラックの容量(kg), W:レンガ一つ当たりの重さ(kg)の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"N W\")");
+                    return;
+                }
+
+                var result = inputArray[0] / inputArray[1];
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/186/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // H:縦のマスの数, W:横のマスの数の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"H W\")");
+                    return;
+                }
+
+                // 各行のブロック数を入力
+                var blockInfo = Enumerable.Range(1, inputArray[0])
+                    .Select(input => Console.ReadLine())
+                    .Select(input => new { blockCount = input.Split(' ').Select(i => int.Parse(i)) })
+                    .SelectMany(input => input.blockCount)
+                    .ToArray();
+
+                var minBlockCount = blockInfo.Min();
+                var result = blockInfo
+                    .Select(blockCount => blockCount - minBlockCount)
+                    .Sum();
+
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
