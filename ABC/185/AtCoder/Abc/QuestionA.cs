ceabf5221c78a6e9323f3d5ea092f8d3a81cfaf0
Ryocham <ryocham.prog@gmail.com>
2021-06-10 01:23:22 +0900

    Add 185 project and Add questionA solution

---
+++ ABC/185/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc185/tasks/abc185_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 問題の案(A1,A2,A3,A4)の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+                var result = inputArray.Min();
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
