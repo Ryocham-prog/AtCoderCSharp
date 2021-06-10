2104a5656b05439b9460bb27a75ef738deec2ef5
Ryocham <ryocham.prog@gmail.com>
2021-06-10 23:52:00 +0900

    Add 184 project and Add questionA,B solution

---
+++ ABC/184/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc184/tasks/abc184_a
+    public class QuestionA
+    {
+        class XYAxis
+        {
+            private int _xAxis;
+            private int _yAxis;
+
+            public XYAxis(int xAxis, int yAxis)
+            {
+                this._xAxis = xAxis;
+                this._yAxis = yAxis;
+            }
+
+            public int GetXAxis()
+            {
+                return _xAxis;
+            }
+
+            public int GetYAxis()
+            {
+                return _yAxis;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 行列情報の入力
+                var inputArray = Enumerable.Range(1, 2)
+                    .Select(x => Console.ReadLine())
+                    .Select(x => new XYAxis(int.Parse(x.Split(' ')[0]), int.Parse(x.Split(' ')[1])))
+                    .ToArray();
+
+                var result = inputArray[0].GetXAxis() * inputArray[1].GetYAxis() - inputArray[0].GetYAxis() * inputArray[1].GetXAxis();
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/184/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc184/tasks/abc184_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:クイズの問題数, X:持ち点の初期値の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"N X\")");
+                    return;
+                }
+
+                var n = inputArray[0];
+                var x = inputArray[1];
+
+                // クイズ結果
+                var quizResult = Console.ReadLine().ToArray();
+
+                var point = x;
+                foreach(var result in quizResult)
+                {
+                    point = (result == 'o') ? point + 1 : (point != 0) ? point - 1 : 0;
+                }
+                Console.WriteLine(point.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
