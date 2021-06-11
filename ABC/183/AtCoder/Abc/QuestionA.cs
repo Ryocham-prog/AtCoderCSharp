18614a70ccdd878c9e9e1e1023176b1acb3e8c78
Ryocham <ryocham.prog@gmail.com>
2021-06-12 00:45:24 +0900

    Add 183 project and Add questionA,B solution

---
+++ ABC/183/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc183/tasks/abc183_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // x:整数の入力
+                var x = int.Parse(Console.ReadLine());
+
+                var result = x < 0 ? 0 : x;
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/183/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc183/tasks/abc183_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // S(x,y), G(x,y)の座標入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => decimal.Parse(i)).ToArray();
+                if (inputArray.Length != 4)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"Sx Sy Gx Gy\")");
+                    return;
+                }
+
+                var sx = inputArray[0];
+                var sy = inputArray[1];
+                var gx = inputArray[2];
+                var gy = inputArray[3];
+
+                var result = (sx * gy + gx * sy) / (sy + gy);
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
