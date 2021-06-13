da46d21c5b0d7fe1d545065ac43141c46699a11d
Ryocham <ryocham.prog@gmail.com>
2021-06-13 23:36:34 +0900

    Add 182 project and Add questionA,B solution

---
+++ ABC/182/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc182/tasks/abc182_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // A:フォロワー, B:フォロー数の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A B\")");
+                    return;
+                }
+
+                var result = (inputArray[0] * 2 + 100) - inputArray[1];
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/182/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc182/tasks/abc182_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:数列Aに含まれる要素数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                // 数列Aの入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != n)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A1 A2 … An\")");
+                    return;
+                }
+
+                var numArray = Enumerable.Range(2, inputArray.Max() - 1).ToArray();
+                var gcdMax = 0;
+                var result = 0;
+                foreach (var num in numArray)
+                {
+                    var gcdArray = inputArray.Where(x => x % num == 0);
+                    if (gcdMax <= gcdArray.Count())
+                    {
+                        gcdMax = gcdArray.Count();
+                        result = num;
+                    }
+                }
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
