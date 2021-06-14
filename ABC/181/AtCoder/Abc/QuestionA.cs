b8601136ce490e8fdf728889eafbe2d55276e133
Ryocham <ryocham.prog@gmail.com>
2021-06-14 22:09:01 +0900

    Add 181 project and Add questionA,B solution

---
+++ ABC/181/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc181/tasks/abc181_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:何色の服を着ているか知りたい日(N日後)
+                var n = int.Parse(Console.ReadLine());
+                var result = n % 2 == 0 ? "White" : "Black" ;
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/181/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc181/tasks/abc181_b
+    class QuestionB
+    {
+        class NumRange
+        {
+            private long _startNum;
+            private long _endNum;
+
+            public long StartNum
+            {
+                set { this._startNum = value; }
+                get { return this._startNum; }
+            }
+            public long EndNum
+            {
+                set { this._endNum = value; }
+                get { return this._endNum; }
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:操作の回数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                var inputArray = Enumerable.Range(1, n)
+                    .Select(x => Console.ReadLine())
+                    .Select(x => new NumRange { StartNum = long.Parse(x.Split(' ')[0]), EndNum = long.Parse(x.Split(' ')[1]) })
+                    .ToArray();
+
+                var result = inputArray
+                    .Select(x => (x.EndNum * (x.EndNum + 1) / 2) - ((x.StartNum - 1) * x.StartNum / 2))
+                    .Sum();
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
