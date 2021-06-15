a0fcbe931e611f7485d4c43ebdf7453d7029f25a
Ryocham <ryocham.prog@gmail.com>
2021-06-15 22:52:55 +0900

    Add 180 project and Add questionA,B solution

---
+++ ABC/180/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc180/tasks/abc180_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:箱に入っていたボールの数, A:取り出したボールの数, B:最後に入れたボールの数の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 3)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"N A B\")");
+                    return;
+                }
+
+                var result = inputArray[0] - inputArray[1] + inputArray[2];
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/180/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc180/tasks/abc180_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:次元の数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                // 点の情報の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => double.Parse(i)).ToArray();
+                if (inputArray.Length != n)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"x1 x2 … xn \")");
+                    return;
+                }
+
+                Console.WriteLine(inputArray.Select(x => Math.Abs(x)).Sum());
+                Console.WriteLine(Math.Sqrt(inputArray.Select(x => Math.Abs(x) * Math.Abs(x)).Sum()));
+                Console.WriteLine(inputArray.Select(x => Math.Abs(x)).Max());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
