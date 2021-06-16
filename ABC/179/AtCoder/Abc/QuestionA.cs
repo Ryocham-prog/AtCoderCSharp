3134433ae641b42b42e3fe91ba50cba74191a9e4
Ryocham <ryocham.prog@gmail.com>
2021-06-17 00:43:07 +0900

    Add 179 project and Add questionA,B solution

---
+++ ABC/179/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc179/tasks/abc179_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // S:単数形の文字列の入力
+                var s = Console.ReadLine();
+                var result = s.Substring(s.Length - 1, 1) == "s" ? s + "es" : s + "s";
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/179/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc179/tasks/abc179_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:サイコロを振る数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                // 二つのサイコロの目の入力
+                var inputArray = Enumerable.Range(1, n)
+                    .Select(x => Console.ReadLine().Split(' '))
+                    .Select((input, index) => new { input, index });
+
+                var sameIndexList = inputArray
+                    .Where(x => x.input[0] == x.input[1])
+                    .Select(x => x.index)
+                    .ToArray();
+
+                var beforeValue = 99;
+                var result = sameIndexList.Aggregate(0, (count, next) =>
+                {
+                    var tmp = count;
+                    if (tmp == 3)
+                    {
+                        return count;
+                    }
+
+                    count = beforeValue + 1 != next ? 1 : count + 1;
+                    beforeValue = next;
+                    return count;
+                });
+
+                var output = result == 3 ? "Yes" : "No";
+                Console.WriteLine(output);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
