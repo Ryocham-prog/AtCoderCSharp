9308469bc1cccc4f7aa41e9529a5b3fbdfced0a9
Ryocham <ryocham.prog@gmail.com>
2021-06-18 00:39:45 +0900

    Add 204 project and Add questionA,B solution

---
+++ ABC/204/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc204/tasks/abc204_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // x:フェネックが出した手, y:アライグマが出した手の入力
+                var inputArray = Console.ReadLine().Split(' ');
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"X Y\")");
+                    return;
+                }
+
+                if (inputArray[0] == inputArray[1])
+                {
+                    Console.WriteLine(inputArray[0]);
+                    return;
+                }
+
+                var result = (inputArray.Where(item => item == "0").Count() == 0) ? "0"
+                    : (inputArray.Where(item => item == "1").Count() == 0) ? "1" : "2" ;
+
+                Console.WriteLine(result);
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/204/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc204/tasks/abc204_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // n:木の本数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                // An：木に成っている木の実の数の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != n)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A1 A2 … An\")");
+                    return;
+                }
+
+                var result = inputArray
+                    .Where(x => x > 10)
+                    .Select(x => x - 10)
+                    .Sum();
+
+                Console.WriteLine(result.ToString());
+                Console.Out.Flush();
+            }
+        }
+    }
+}
