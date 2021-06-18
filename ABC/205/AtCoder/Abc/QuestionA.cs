63a5312b609017e8bc2dc7bbb5b413797a1d6df8
Ryocham <ryocham.prog@gmail.com>
2021-06-18 21:51:26 +0900

    Add 205 project and Add questionA,B solution

---
+++ ABC/205/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc205/tasks/abc205_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // A:100mLあたりのエネルギー(kcal), B:ドリンクの量(mL)の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => double.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A B\")");
+                    return;
+                }
+                var result = inputArray[0] * inputArray[1] / 100;
+
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/205/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc205/tasks/abc205_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:数列の長さの入力
+                var n = int.Parse(Console.ReadLine());
+
+                // 数列Nの入力
+                var inputArray = Console.ReadLine().Split(' ')
+                    .Select(i => int.Parse(i))
+                    .OrderBy(x => x)
+                    .ToArray();
+
+                var result = inputArray.SequenceEqual(Enumerable.Range(1, n).ToArray()) ? "Yes" : "No";
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
