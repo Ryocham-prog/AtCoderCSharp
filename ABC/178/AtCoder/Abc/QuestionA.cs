03f5307c954d40ec5adce654eb4d2a323a265504
Ryocham <ryocham.prog@gmail.com>
2021-06-19 22:36:28 +0900

    Add 178 project and Add questionA,B solution

---
+++ ABC/178/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc178/tasks/abc178_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数の入力
+                var n = int.Parse(Console.ReadLine());
+                var result = n == 0 ? "1" : "0";
+
+                Console.WriteLine(result);
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/178/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc178/tasks/abc178_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数a,b,c,dの入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+                if (inputArray.Length != 4)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"a b c d\")");
+                    return;
+                }
+
+                var result = inputArray
+                    .Select((val, index) => new { val, index })
+                    .Aggregate(long.MinValue, (max, next) => {
+                        if (next.index > 1) return max;
+
+                        var maxXY = Math.Max(next.val * inputArray[2], next.val * inputArray[3]);
+
+                        return Math.Max(max, maxXY);
+                    });
+
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
