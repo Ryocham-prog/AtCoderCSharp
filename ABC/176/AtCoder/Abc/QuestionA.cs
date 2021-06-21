241bca0b852122cf0f9f6ab0b4bdabfe92526284
Ryocham <ryocham.prog@gmail.com>
2021-06-21 22:31:19 +0900

    Add 176 project and Add questionA,B solution

---
+++ ABC/176/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc176/tasks/abc176_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:作りたいたこ焼きの数, X:一度に作れる数, T:1セット作る時間の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 3)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"N X T\")");
+                    return;
+                }
+
+                var tmp = inputArray[0] / inputArray[1];
+                var result = inputArray[0] % inputArray[1] != 0 ? (tmp + 1) * inputArray[2] : tmp * inputArray[2];
+
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/176/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc176/tasks/abc176_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数Nの入力
+                var s = Console.ReadLine();
+
+                var sumNum = s.ToArray()
+                    .Select(x => int.Parse(x.ToString()))
+                    .Sum();
+
+                var result = sumNum % 9 == 0 ? "Yes" : "No";
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
