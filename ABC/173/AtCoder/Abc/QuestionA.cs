17adc6663c1cfbeb36292437ce6193306d0d8a0a
Ryocham <ryocham.prog@gmail.com>
2021-06-25 02:38:03 +0900

    Add 173 project and Add questionA,B solution

---
+++ ABC/173/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc173/tasks/abc173_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:商品の値段の入力
+                var n = int.Parse(Console.ReadLine());
+
+                var tmp = n % 1000 != 0 ? n / 1000 + 1 : n / 1000;
+                var result = 1000 * tmp - n;
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/173/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc173/tasks/abc173_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:テストケースの数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                // S:ジャッジ結果の入力
+                var resultList = Enumerable.Range(1, n)
+                    .Select(x => Console.ReadLine())
+                    .GroupBy(x => x)
+                    .Select(x => new { val = x.Key, count = x.Count() })
+                    .ToDictionary(x => x.val, x => x.count);
+
+                var acResult = resultList.ContainsKey("AC") ? resultList["AC"] : 0;
+                Console.WriteLine("AC x " + acResult);
+
+                var waResult = resultList.ContainsKey("WA") ? resultList["WA"] : 0;
+                Console.WriteLine("WA x " + waResult);
+
+                var tleResult = resultList.ContainsKey("TLE") ? resultList["TLE"] : 0;
+                Console.WriteLine("TLE x " + tleResult);
+
+                var reResult = resultList.ContainsKey("RE") ? resultList["RE"] : 0;
+                Console.WriteLine("RE x " + reResult);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
