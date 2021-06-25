b5c9c75a85a73bf699848b30cbbcb85a234390a0
Ryocham <ryocham.prog@gmail.com>
2021-06-26 02:59:08 +0900

    Add 172 project and Add questionA,B solution

---
+++ ABC/172/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc172/tasks/abc172_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数aの入力
+                var a = int.Parse(Console.ReadLine());
+
+                var result = a + Math.Pow(a, 2) + Math.Pow(a, 3);
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/172/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc172/tasks/abc172_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 文字列Sの入力
+                var s = Console.ReadLine().ToArray();
+                // 文字列Tの入力
+                var t = Console.ReadLine().ToArray();
+
+                var result = s.Where((x, index) => x != t[index]).Count();
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+
+            }
+        }
+    }
+}
