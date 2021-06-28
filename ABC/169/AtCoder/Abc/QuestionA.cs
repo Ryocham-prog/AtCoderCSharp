5fa2e41ed57f4104f0187ae117d00b16053f8b28
Ryocham <ryocham.prog@gmail.com>
2021-06-28 23:12:00 +0900

    Add 169 project and Add questionA,B solution

---
+++ ABC/169/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc169/tasks/abc169_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数配列の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A B\")");
+                    return;
+                }
+
+                var result = inputArray[0] * inputArray[1];
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/169/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc169/tasks/abc169_b
+    class QuestionB
+    {
+        const long maxVal = 1000000000000000000;
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:整数の個数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                // 整数配列の入力
+                var inputLongArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+
+                if(inputLongArray.Where(x => x == 0).Count() > 0)
+                {
+                    Console.WriteLine(0);
+                    return;
+                }
+
+                var result = inputLongArray.Aggregate(1L, (pro, next) =>
+                {
+                    var tmp = pro;
+                    if (tmp == -1) return tmp;
+
+                    tmp = tmp > maxVal / next ? -1 : tmp * next;
+
+                    return tmp;
+                });
+                Console.WriteLine(result.ToString());
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
