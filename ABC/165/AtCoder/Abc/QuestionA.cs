12005f0f2746fdedc1035d126a1cad0649781fcf
Ryocham <ryocham.prog@gmail.com>
2021-07-02 23:59:19 +0900

    Add 165 project and Add questionA,B solution

---
+++ ABC/165/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc165/tasks/abc165_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // K:目標のもとになる数の入力
+                var n = long.Parse(Console.ReadLine());
+
+                // A,B:出せる距離の範囲(A以上B以下)の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A B\")");
+                    return;
+                }
+
+                var a = inputArray[0];
+                var b = inputArray[1];
+
+                var tmp = (a % n == 0) ? (a / n) : (a / n + 1);
+                var result = tmp * n <= b ? "OK" : "NG";
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/165/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc165/tasks/abc165_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // X:預金判定額の入力
+                var x = long.Parse(Console.ReadLine());
+
+                var count = 0;
+                decimal val = 100;
+                while(val < x)
+                {
+                    val+= Math.Floor(val / 100);
+                    count++;
+                }
+
+                Console.WriteLine(count);
+                Console.Out.Flush();
+            }
+        }
+
+        // 99-after-contest-01.txtがWrong Answerなので、後日検討 
+        /*
+          public long Calc(double val, long x, long count)
+        {
+             if(val >= x) return count;
+         
+             val += Math.Floor(val / 100);
+             count++;
+ 
+              return Calc(val, x, count);
+         }
+        */
+    }
+}
