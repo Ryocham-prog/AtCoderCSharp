20d499f34fa55b791abc9c5da38d19095d1f1c43
Ryocham <ryocham.prog@gmail.com>
2021-07-03 22:46:50 +0900

    Add 164 project and Add questionA,B solution

---
+++ ABC/164/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc164/tasks/abc164_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // S:羊の数, W:狼の数の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"S W\")");
+                    return;
+                }
+
+                var result = inputArray[0] <= inputArray[1] ? "unsafe" : "safe ";
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/164/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc164/tasks/abc164_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // A, B, C, Dの入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 4)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A B C D\")");
+                    return;
+                }
+
+                var a = inputArray[0];
+                var b = inputArray[1];
+                var c = inputArray[2];
+                var d = inputArray[3];
+
+                // 倒すまでのターン
+                var turnT = ((c % b) == 0) ? (c / b) : ((c / b) + 1);
+                var turnA = ((a % d) == 0) ? (a / d) : ((a / d) + 1);
+
+                var result = turnT <= turnA ? "Yes" : "No";
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
