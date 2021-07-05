82ac8729596e6d6f93cf5303b08691cbbb3fc2ba
Ryocham <ryocham.prog@gmail.com>
2021-07-05 23:12:19 +0900

    Add 162 project and Add questionA,B solution

---
+++ ABC/162/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc162/tasks/abc162_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:3桁の整数の入力
+                var s = Console.ReadLine();
+
+                var result = s.Contains("7") ? "Yes" : "No";
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/162/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc162/tasks/abc162_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:項目数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                var targetArraySum = Enumerable.Range(1, n)
+                    .Aggregate(0L,(sum, next) => {
+                        if (next % 3 == 0 || next % 5 == 0) return sum;
+
+                        return sum += next;
+                    });
+
+                Console.WriteLine(targetArraySum);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
