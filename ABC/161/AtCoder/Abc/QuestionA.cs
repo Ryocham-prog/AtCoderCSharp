56edf5b07ce6d4436326dcebcea9ab4be18070f5
Ryocham <ryocham.prog@gmail.com>
2021-07-07 00:01:22 +0900

    Add 161 project and Add questionA,B solution

---
+++ ABC/161/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc161/tasks/abc161_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 箱A, B, Cに入った整数X, Y, Zの入力
+                var inputArray = Console.ReadLine().Split(' ');
+                if (inputArray.Length != 3)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"X Y Z\")");
+                    return;
+                }
+
+                // AとBを入れ替え
+                var tmp = inputArray[1];
+                inputArray[1] = inputArray[0];
+                inputArray[0] = tmp;
+
+                tmp = inputArray[2];
+                inputArray[2] = inputArray[0];
+                inputArray[0] = tmp;
+
+                Console.WriteLine(string.Join(" ", inputArray));
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/161/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc161/tasks/abc161_a
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:商品の種類, M:選ぶ人気商品の数の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"N M\")");
+                    return;
+                }
+
+                var n = inputArray[0];
+                var m = inputArray[1];
+
+                var voteInfoArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i))
+                    .OrderByDescending(x => x)
+                    .ToArray();
+
+                var checkVal = (decimal)voteInfoArray.Sum() / (decimal)(4 * m);
+
+                // Aggregate利用
+                var result = voteInfoArray.Take(m)
+                                    .Aggregate(true, (judge, next) =>{
+                                        if (!judge) return judge;
+
+                                        return next >= checkVal;
+                                    });
+
+                var output = result ? "Yes" : "No";
+
+                // Aggregate利用しない
+
+                /*                var result = voteInfoArray
+                                    .Take(m)
+                                    .OrderBy(x => x)
+                                    .ElementAtOrDefault(0);
+
+                                var output = result >= checkVal ? "Yes" : "No";
+                */
+                Console.WriteLine(output);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
