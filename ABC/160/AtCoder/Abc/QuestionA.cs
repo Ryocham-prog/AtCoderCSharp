ae32c0d0774314c8e202636eebe5226cc5e4be2e
Ryocham <ryocham.prog@gmail.com>
2021-07-08 00:23:30 +0900

    Add 160 project and Add questionA,B solution

---
+++ ABC/160/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc160/tasks/abc160_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // S:文字列の入力
+                var charArray = Console.ReadLine();
+                if (charArray.Length != 6)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力値の長さ：\"6\")");
+                    return;
+                }
+
+                var result = (charArray[2] == charArray[3] && charArray[4] == charArray[5])
+                    ? "Yes" : "No";
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/160/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc160/tasks/abc160_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                var thousandCount = n / 500;
+                var hundredCount = (n - 500 * thousandCount) / 5;
+
+                var result = (1000 * thousandCount) + (5 * hundredCount);
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
