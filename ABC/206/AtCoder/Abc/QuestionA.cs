f824157ca5cc7017ffbe5c6ca028fa30774d6b21
Ryocham <ryocham.prog@gmail.com>
2021-07-10 02:43:53 +0900

    Add 206 project and Add questionA,B solution

---
+++ ABC/206/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc206/tasks/abc206_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:税抜き価格の入力
+                var n = double.Parse(Console.ReadLine());
+
+                var price = Math.Floor(1.08 * n);
+                var result = price < 206 ? "Yay!" : 
+                    price == 206 ? "so-so" : ":(";
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/206/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc206/tasks/abc206_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                var q = new QuestionB();
+                var output = q.Add(n, 0, 1);
+
+                Console.WriteLine(output);
+
+                Console.Out.Flush();
+            }
+        }
+
+        private int Add(int n, int savings, int days)
+        {
+            if (savings >= n) return days - 1;
+
+            savings += days;
+            days++;
+            var result = Add(n, savings, days);
+
+            return result;
+        }
+    }
+}
