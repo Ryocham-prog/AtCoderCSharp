1153a0d5ed4945f803befb76a2aaddf19609a24b
Ryocham <ryocham.prog@gmail.com>
2021-07-11 20:28:32 +0900

    Add 208 project and Add questionA,B solution

---
+++ ABC/208/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc208/tasks/abc208_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // A:サイコロを振る回数, B:出た目の合計期待値の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A B\")");
+                    return;
+                }
+
+
+                var a = inputArray[0];
+                var b = inputArray[1];
+
+                var result = ((1 * a) <= b) && (b <= (6 * a)) ? "Yes" : "No" ;
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/208/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc208/tasks/abc208_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // P:商品の価格の入力
+                var p = int.Parse(Console.ReadLine());
+
+                var coinArray = Enumerable.Range(1, 10)
+                    //.Select((val, index) => new { val, index })
+                    .Select(x => {
+                        return Enumerable.Range(1, x)
+                        .Aggregate(1, (product, next) =>
+                        {
+                            return product * next;
+                        });
+                      })
+                    .OrderByDescending(x => x)
+                    .ToArray();
+
+                var result = coinArray
+                    .Aggregate(new { coinCnt = 0, payment = p }, (info, next) =>
+                    {
+                        if (info.payment == 0) return info;
+
+                        var tmp = info.payment / next;
+                        if (tmp > 100) return new { coinCnt = info.coinCnt + 100, payment = info.payment - next * 100 };
+
+
+                        return new { coinCnt = info.coinCnt + tmp, payment = info.payment - next * tmp };
+                    })
+                    .coinCnt;
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
