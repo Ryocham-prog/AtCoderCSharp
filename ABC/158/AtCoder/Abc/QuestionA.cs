e04283b8f891636003d4b22ac7492803cba119a1
Ryocham <ryocham.prog@gmail.com>
2021-07-14 01:08:06 +0900

    Add 158 project and Add questionA,B solution

---
+++ ABC/158/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc158/tasks/abc158_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 文字列の入力
+                var s = Console.ReadLine();
+
+                var result = s.ToArray().GroupBy(x => x).Count() > 1 ? "Yes" : "No" ;
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/158/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc158/tasks/abc158_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:先頭から取得するボールの数
+                // A:加える青いボールの数
+                // B:加える赤いボールの数の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => long.Parse(i)).ToArray();
+                if (inputArray.Length != 3)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"N A B\")");
+                    return;
+                }
+
+                var n = inputArray[0];
+                var a = inputArray[1];
+                var b = inputArray[2];
+
+                // 取得するボールの数を操作
+                var blueQuo = (n / (a + b)) * a ;
+
+                var blueRem = (n % (a + b)) < a ? (n % (a + b)) : a ;
+
+
+                Console.WriteLine(blueQuo + blueRem);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
