e6483375498cce0571cbc72c0611a174160292c1
Ryocham <ryocham.prog@gmail.com>
2021-07-15 02:48:48 +0900

    Add 157 project and Add questionA solution

---
+++ ABC/157/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc157/tasks/abc157_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数の入力
+                var n = int.Parse(Console.ReadLine());
+
+                var result = (n % 2 == 1) ? (n / 2 + 1) : (n / 2);
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
