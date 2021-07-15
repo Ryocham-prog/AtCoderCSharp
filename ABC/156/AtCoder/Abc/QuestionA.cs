66775a4ae65fb83080512275120258796ecc649f
Ryocham <ryocham.prog@gmail.com>
2021-07-16 01:14:00 +0900

    Add 156 project and Add questionA,B solution

---
+++ ABC/156/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc156/tasks/abc156_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:コンテスト参加回数, R:表示レーティングの入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"N R\")");
+                    return;
+                }
+
+                var n = inputArray[0];
+                var r = inputArray[1];
+
+                var result = n >= 10 ? r : r + (100 * (10 - n));
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/156/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc156/tasks/abc156_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:整数, K:進数の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"N K\")");
+                    return;
+                }
+
+                var n = inputArray[0];
+                var k = inputArray[1];
+
+                var result = 0;
+                while(n > 0)
+                {
+                    n /= k;
+                    result++;
+                }
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
