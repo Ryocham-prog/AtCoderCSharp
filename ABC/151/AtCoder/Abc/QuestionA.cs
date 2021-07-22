08d7e6d22635193067b88502468955663fba05a1
Ryocham <ryocham.prog@gmail.com>
2021-07-22 18:06:34 +0900

    Add 151 project and Add questionA,B solution

---
+++ ABC/151/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc151/tasks/abc151_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // C:英小文字の入力
+                var c = Convert.ToChar(Console.ReadLine());
+
+                var result = (char)(c + 1);
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+
+            }
+        }
+    }
+}
---
+++ ABC/151/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc151/tasks/abc151_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:科目の数, K:テストの満点, M:目標平均点の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 3)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"N K M\")");
+                    return;
+                }
+
+                var n = inputArray[0];
+                var k = inputArray[1];
+                var m = inputArray[2];
+
+                var scoreArray = Console.ReadLine().Split(' ')
+                    .Select(i => int.Parse(i))
+                    .ToArray();
+
+                var diff = m * n - scoreArray.Sum();
+                var result = (diff > k) ? -1
+                    : (diff < 0) ? 0 : diff;
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
