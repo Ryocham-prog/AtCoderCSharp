1acd86eb9736c84c44ee7031da6af04afc46dd1d
Ryocham <ryocham.prog@gmail.com>
2021-07-23 15:32:45 +0900

    Add 150 project and Add questionA,B solution

---
+++ ABC/150/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc150/tasks/abc150_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // K:500円の枚数, X:判定の金額の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"K X\")");
+                    return;
+                }
+
+                var k = inputArray[0];
+                var x = inputArray[1];
+
+                var result = 500 * k >= x ? "Yes" : "No";
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/150/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc150/tasks/abc150_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // N:文字列の長さの入力
+                Int32.TryParse(Console.ReadLine(), out int n);
+
+                // S:文字列の入力
+                var s = Console.ReadLine();
+
+                var replaceStr = s.Replace("ABC", "");
+                var result = (s.Length - replaceStr.Length) / 3;
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
