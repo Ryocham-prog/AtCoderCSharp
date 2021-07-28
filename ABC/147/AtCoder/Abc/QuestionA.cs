83cbfa9d67584887c7e47a8d0009007e1c1ab54b
Ryocham <ryocham.prog@gmail.com>
2021-07-28 20:28:23 +0900

    Add 147 project and Add questionA,B solution

---
+++ ABC/147/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc147/tasks/abc147_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数配列の入力
+                var inputArray = Console.ReadLine().Split(' ').Select(i => int.Parse(i)).ToArray();
+                var tmp = inputArray.Sum() ;
+                var result = tmp < 22 ? "win" : "bust";
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/147/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc147/tasks/abc147_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 文字列の入力
+                var s = Console.ReadLine();
+
+                var len = s.Length;
+                var result = Enumerable.Range(0, len / 2)
+                    .Aggregate(0, (count, next) =>
+                    {
+                        var headStr = s.Substring(next, 1);
+                        var tailStr = s.Substring(len - 1 - next, 1);
+
+                        return headStr != tailStr ? count + 1 : count;
+                    });
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
