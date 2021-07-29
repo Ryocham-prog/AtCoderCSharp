a819fe8b5b2eaa9bba28d783a3368f2355e4f8d3
Ryocham <ryocham.prog@gmail.com>
2021-07-30 02:04:13 +0900

    Add 146 project and Add questionA,B solution

---
+++ ABC/146/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc146/tasks/abc146_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 文字列の入力
+                var s = Console.ReadLine();
+
+                var tmp = s == "SUN" ? 0
+                    : s == "MON" ? 1
+                    : s == "TUE" ? 2
+                    : s == "WED" ? 3
+                    : s == "THU" ? 4
+                    : s == "FRI" ? 5 : 6;
+
+                var result = 7 - tmp;
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/146/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc146/tasks/abc146_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数の入力
+                Int32.TryParse(Console.ReadLine(), out int n);
+
+                // 文字列の入力
+                var s = Console.ReadLine();
+
+                var result = s.ToArray()
+                    .Aggregate(new StringBuilder(), (output, next) =>
+                    {
+                        var tmp = next - 'A' + n;
+                        if (tmp >= 26) tmp %= 26;
+                        return output.Append((char)(tmp + 'A'));
+                    }).ToString();
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
