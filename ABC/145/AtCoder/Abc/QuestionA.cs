48d685caaf5bdd91e0531f636c604a99a8542cb8
Ryocham <ryocham.prog@gmail.com>
2021-07-30 22:15:54 +0900

    Add 145 project and Add questionA,B solution

---
+++ ABC/145/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc145/tasks/abc145_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数の入力
+                Int32.TryParse(Console.ReadLine(), out int n);
+
+                Console.WriteLine(Math.Pow(n, 2));
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/145/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc145/tasks/abc145_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数の入力
+                Int32.TryParse(Console.ReadLine(), out int n);
+
+                // 文字列の入力
+                var s = Console.ReadLine();
+                if (n % 2 == 1)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+                var result = (s.Substring(0, n / 2) == s.Substring(n / 2, n / 2)) 
+                    ? "Yes" : "No";
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
