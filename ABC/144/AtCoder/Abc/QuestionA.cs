1019943762c687853f47bb251269006634b65d5e
Ryocham <ryocham.prog@gmail.com>
2021-07-31 22:50:50 +0900

    Add 144 project and Add questionA,B solution

---
+++ ABC/144/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc144/tasks/abc144_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 2つの整数A,Bの入力
+                var inputArray = Console.ReadLine().Split(' ')
+                    .Select(i => {
+                        Int32.TryParse(i, out int tmp);
+                        return tmp;
+                    }).ToArray();
+
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A B\")");
+                    return;
+                }
+
+                var a = inputArray[0];
+                var b = inputArray[1];
+
+                var result = (a < 1 || 9 < a || b < 1 || 9 < b) ? -1 : a * b;
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/144/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc144/tasks/abc144_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数Nの入力
+                Int32.TryParse(Console.ReadLine(), out int n);
+
+                var result = Enumerable.Range(1, 9)
+                    .Aggregate(false, (judge, next) =>
+                    {
+                        if (judge) return judge;
+
+                        var result2 = Enumerable.Range(1, 9)
+                        .Aggregate(false, (judge2, next2) =>
+                        {
+                            if (judge2) return judge2;
+
+                            return next * next2 == n;
+                        });
+
+                        return result2;
+                    });
+
+                var output = result ? "Yes" : "No";
+                Console.WriteLine(output);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
