697d70dd1ab11d8dc89e05bd6747747939d3a8f5
Ryocham <ryocham.prog@gmail.com>
2021-08-02 23:39:35 +0900

    Add 211 project and Add questionA,B solution

---
+++ ABC/211/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc211/tasks/abc211_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数配列の入力
+                var inputArray = Console.ReadLine().Split(' ')
+                    .Select(i => {
+                        var success = Decimal.TryParse(i, out Decimal tmpVal);
+                        return new { success, val = tmpVal };
+                    }).ToArray();
+
+                if (inputArray.Where(x => !(x.success)).Count() > 0)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"50 < B <= A <= 300\")");
+                    return;
+                }
+
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A B\")");
+                    return;
+                }
+
+                var a = inputArray[0].val;
+                var b = inputArray[1].val;
+
+                Console.WriteLine((a + 2 * b) / 3);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/211/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc211/tasks/abc211_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                var strList = Enumerable.Range(1, 4)
+                    .Select(x => Console.ReadLine())
+                    .ToArray();
+
+                // 制約により、H , 2B , 3B , HR のいずれかと一致することが確定しているので↓
+                var result = strList.GroupBy(x => x).Count() == 4 ? "Yes" : "No";
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
