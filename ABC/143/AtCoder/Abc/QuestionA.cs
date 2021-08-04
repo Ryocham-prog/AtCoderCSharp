a5099a4aa9d1e014adb0799f0c28623c63b2aa1a
Ryocham <ryocham.prog@gmail.com>
2021-08-04 20:43:03 +0900

    Add 143 project and Add questionA,B solution

---
+++ ABC/143/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc143/tasks/abc143_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数配列の入力
+                var inputArray = Console.ReadLine().Split(' ')
+                    .Select(i => {
+                        var success = Int32.TryParse(i, out int tmpVal);
+                        return new { success, val = tmpVal };
+                    }).ToArray();
+
+                if(inputArray.Where(x => !(x.success)).Count() > 0)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"1 <= A,B <= 100\")");
+                    return;
+                }
+
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"A B\")");
+                    return;
+                }
+                var a = inputArray[0].val;
+                var b = inputArray[1].val;
+
+                var result = a <= (2 * b) ? 0 : a - (2 * b);
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/143/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc143/tasks/abc143_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数の入力
+                var success = Int32.TryParse(Console.ReadLine(), out int n);
+                if(!success)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"2 <= n <= 50\")");
+                    return;
+                }
+
+                // 整数配列の入力
+                var inputArray = Console.ReadLine().Split(' ')
+                    .Select(i => {
+                        var success1 = Int32.TryParse(i, out int tmpVal);
+                        return new { success = success1, val = tmpVal };
+                    }).ToArray();
+                if(inputArray.Where(x => !(x.success)).Count() > 0)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"0 <= d <= 100\")");
+                    return;
+                }
+
+                var takoInfoList = inputArray.Select(x => x.val).ToArray();
+
+                var selectionList = takoInfoList
+                    .SelectMany((x, index) =>
+                    {
+                        return takoInfoList.Skip(index + 1).Select(y => new List<int> { x, y });
+                    });
+
+                var result =selectionList
+                    .Select(val => val[0] * val[1])
+                    .Sum();
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
