a71a6d54fd95fe7db5c5f76ddde17cae6356da5c
Ryocham <ryocham.prog@gmail.com>
2021-08-05 21:29:21 +0900

    Add 142 project and Add questionA,B solution

---
+++ ABC/142/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc142/tasks/abc142_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数の入力
+                if (!Int32.TryParse(Console.ReadLine(), out int n))
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"1 <= n <= 100\")");
+                    return;
+                }
+
+                var intList = Enumerable.Range(1, n).ToArray();
+                var oddList = intList.Where(x => x % 2 == 1);
+
+                var result = (decimal)oddList.Count() / (decimal)intList.Count();
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/142/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc142/tasks/abc142_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数配列の入力
+                var inputArray = Console.ReadLine().Split(' ')
+                    .Select(i => {
+                        var success = Int32.TryParse(i, out int tmpVal);
+                        return new { success, val = tmpVal };
+                    }).ToArray();
+
+                if(inputArray.Where(x => !(x.success)).Count() > 0)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"1 <= N <= 100000, 1 <= K <= 500\")");
+                    return;
+                }
+
+                if (inputArray.Length != 2)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"N K\")");
+                    return;
+                }
+                var n = inputArray[0].val;
+                var k = inputArray[1].val;
+
+                var inputList2 = Console.ReadLine().Split(' ')
+                    .Select(i => {
+                        var success = Int32.TryParse(i, out int tmpVal2);
+                        return new { success, val = tmpVal2 };
+                    }).ToArray();
+
+                if (inputList2.Where(x => !(x.success)).Count() > 0)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"1 <= h <= 500\")");
+                    return;
+                }
+
+                var heightList = inputList2
+                    .Select(x => x.val)
+                    .ToArray();
+
+                var result = heightList
+                    .Where(x => x >= k)
+                    .Count();
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
