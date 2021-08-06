bd9d3865424fa2f41fbe24d7e5486aeaea1e6981
Ryocham <ryocham.prog@gmail.com>
2021-08-06 21:57:52 +0900

    Add 141 project and Add questionA,B solution

---
+++ ABC/141/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc141/tasks/abc141_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 文字列の入力
+                var s = Console.ReadLine();
+
+                var result = s == "Sunny" ? "Cloudy"
+                    : s == "Cloudy" ? "Rainy" : "Sunny";
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/141/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc141/tasks/abc141_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 文字列の入力
+                var s = Console.ReadLine();
+
+                var targetArray = s.ToArray()
+                    .Select((x, index) => new { val = x, index = index + 1 });
+
+                // 奇数番目の判定
+                var oddBool = targetArray
+                    .Where(x => x.index % 2 == 1)
+                    .Aggregate(true, (judge, next) =>
+                    {
+                        if (judge == false) return judge;
+                        return ((next.val == 'R') || (next.val == 'U') || (next.val == 'D'));
+                    });
+
+                if(!oddBool)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+                // 偶数番目の判定
+                var evenBool = targetArray
+                    .Where(x => x.index % 2 == 0)
+                    .Aggregate(true, (judge, next) =>
+                    {
+                        if (judge == false) return judge;
+                        return ((next.val == 'L') || (next.val == 'U') || (next.val == 'D'));
+                    });
+
+                if (!evenBool)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+                Console.WriteLine("Yes");
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
