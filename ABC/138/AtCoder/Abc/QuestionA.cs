c9c66b4e4965265a8f2b8d64dd2470f87c9e4dfc
Ryocham <ryocham.prog@gmail.com>
2021-08-10 19:49:25 +0900

    Add 138 project and Add questionA,B solution

---
+++ ABC/138/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc138/tasks/abc138_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数の入力
+                if(!Int32.TryParse(Console.ReadLine(), out int a))
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"2800 <= a <= 5000\")");
+                    return;
+                }
+
+                // 文字列の入力
+                var s = Console.ReadLine();
+
+                var result = a >= 3200 ? s : "red";
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/138/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    // https://atcoder.jp/contests/abc138/tasks/abc138_b
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数の入力
+                if(!Int32.TryParse(Console.ReadLine(), out int n))
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"1 <= n <= 100\")");
+                    return;
+                }
+
+                // 整数配列の入力
+                var inputArray = Console.ReadLine().Split(' ')
+                    .Select(i => {
+                        var success = decimal.TryParse(i, out decimal tmpVal);
+                        return new { success, val = tmpVal };
+                    }).ToArray();
+
+                if (inputArray.Where(x => !(x.success)).Count() > 0)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"1 <= Ai <= 1000\")");
+                    return;
+                }
+
+                var sumReverse = inputArray.Select(x => x.val)
+                    .Aggregate(0m, (sum, next) =>
+                    {
+                        return sum + (1 / next);
+                    });
+
+                var result = 1 / sumReverse;
+
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
