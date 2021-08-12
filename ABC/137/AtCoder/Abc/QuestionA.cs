b7a431f06c843114054d058b37af33f6ac96657b
Ryocham <ryocham.prog@gmail.com>
2021-08-13 02:14:12 +0900

    Add 137 project and Add questionA,B solution

---
+++ ABC/137/AtCoder/Abc/QuestionA.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    //https://atcoder.jp/contests/abc141/tasks/abc141_a
+    public class QuestionA
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数配列の入力
+                var inputArray = Console.ReadLine().Split(' ')
+                    .Select(i => {
+                        var success = Int32.TryParse(i, out int tmpVal);
+                        return new { success, val = tmpVal };
+                    }).ToArray();
+
+                if (inputArray.Where(x => !(x.success)).Count() > 0)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"-100 <= A,B <= 100\")");
+                    return;
+                }
+
+                var a = inputArray[0].val;
+                var b = inputArray[1].val;
+
+                var tmp = Math.Max(a + b, a - b);
+                var result = Math.Max(a * b, tmp);
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
---
+++ ABC/137/AtCoder/Abc/QuestionB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoder.Abc
+{
+    class QuestionB
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new System.IO.StreamWriter(Console.OpenStandardOutput()))
+            {
+                Console.SetOut(sw);
+
+                // 整数配列の入力
+                var inputArray = Console.ReadLine().Split(' ')
+                    .Select(i => {
+                        var success = Int32.TryParse(i, out int tmpVal);
+                        return new { success, val = tmpVal };
+                    }).ToArray();
+
+                if (inputArray.Where(a => !(a.success)).Count() > 0)
+                {
+                    Console.Error.WriteLine("入力値を確認してください。(入力形式：\"1 <= K <= 100, 0 <= X <= 100\")");
+                    return;
+                }
+
+                var k = inputArray[0].val;
+                var x = inputArray[1].val;
+
+                var negaVal = x - (k - 1) < -1000000 ? -1000000 : x - (k - 1);
+                var posiVal = x + (k - 1) > 1000000 ? 1000000 : x + (k - 1);
+
+                var result = String.Join(" ", Enumerable.Range(negaVal, posiVal - negaVal + 1));
+                Console.WriteLine(result);
+
+                Console.Out.Flush();
+            }
+        }
+    }
+}
